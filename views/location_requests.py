6a2f9ff7d5ced3b953f4d89099da04d12802f289
Pavankumar Patel <patelmedic2@gmail.com>
2023-01-12 14:51:36 -0600

    first commit for kennels

---
+++ views/location_requests.py
@@ -0,0 +1,65 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville North",
+        "address": "8422 Johnson Pike"
+    },
+    {
+        "id": 2,
+        "name": "Nashville South",
+        "address": "209 Emory Drive"
+    }
+]
+
+def get_all_locations():
+    return LOCATIONS
+
+#function with a single paramater
+def get_single_location(id):
+    #variavle to hold found location
+    requested_location = None
+
+    # Iterate the LOCATIONS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for location in LOCATIONS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if location["id"] == id:
+            requested_location = location
+
+    return requested_location
+
+def create_location(location):
+    max_id = LOCATIONS[-1]["id"]
+
+    new_id = max_id + 1
+
+    location["id"] = new_id
+    LOCATIONS.append(location)
+
+    return location
+
+def delete_location(id):
+    # Initial -1 value for location index, in case one isn't found
+    location_index = -1
+
+    # Iterate the LOCATIONS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, location in enumerate(LOCATIONS):
+        if location["id"] == id:
+            # Found the location. Store the current index.
+            location_index = index
+
+    # If the location was found, use pop(int) to remove it from list
+    if location_index >= 0:
+        LOCATIONS.pop(location_index)
+
+
+def update_location(id, new_location):
+    # Iterate the LOCATIONS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, location in enumerate(LOCATIONS):
+        if location["id"] == id:
+            # Found the location. Update the value.
+            LOCATIONS[index] = new_location
+            break
